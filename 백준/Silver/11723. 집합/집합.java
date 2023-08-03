b1a19118f21c9e74be7919ea6470bfcd9896798a
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2023-08-03 15:59:16 +0900

    [Silver V] Title: 집합, Time: 1248 ms, Memory: 299676 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,52 +1,62 @@
-import java.io.*;
-import java.util.*;
 
-class Main {
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
-		boolean[] arr=new boolean[20];
-		int M=Integer.parseInt(br.readLine());
-		for(int i=0;i<M;i++)
+		int N=Integer.parseInt(br.readLine());
+		int S=0;
+		
+		for(int i=0;i<N;i++)
 		{
 			String[] tmp=br.readLine().split(" ");
-			int n=0;
-			if(tmp.length!=1)
-				n=Integer.parseInt(tmp[1])-1;
-			switch (tmp[0]) {
-				case ("add"):
-					arr[n]=true;
-					break;
-				case ("remove"):
-					arr[n]=false;
-					break;
-				case ("check"):
-					if(arr[n])
-						bw.write(1+"\n");
-					else
-						bw.write(0+"\n");
-					break;
-				case ("toggle"):
-					arr[n]=!(arr[n]);
-					break;
-				case ("all"):
-					for(int j=0;j<20;j++)
-					{
-						arr[j]=true;
-					}
-					break;
-				case ("empty"):
-					for(int j=0;j<20;j++)
-					{
-						arr[j]=false;
-					}
-					break;
+			int n;
+			switch(tmp[0])
+			{
+			case "add":
+				n=Integer.parseInt(tmp[1]);
+				S=S|1<<(n-1);
+				break;
+			case "remove":
+				n=Integer.parseInt(tmp[1]);
+				S=S&~(1<<n-1);
+				break;
+			case "check":
+				n=Integer.parseInt(tmp[1]);
+	
+				if((S&(1<<n-1))>>(n-1)==1)
+				{
+					bw.write("1\n");
 				}
+				else
+				{
+					bw.write("0\n");
+				}
+				break;
+			case "toggle":
+				n=Integer.parseInt(tmp[1]);
+				//System.out.println(Integer.toBinaryString(S));
+				if((S&(1<<n-1))>>(n-1)==1)
+				{
+					S=S&~(1<<n-1);
+				}
+				else
+				{
+					S=S|1<<(n-1);
+				}
+				break;
+			case "all":
+				S=(1<<20)-1;
+				break;
+			case "empty":
+				S=0;
+				break;
+			}
+			
 		}
-		
 		bw.flush();
-		br.close();
-		bw.close();
 	}
 }
