2067d33098ba01711de2ba5a83a6e18ab3a4db56
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2023-08-04 17:20:29 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45408 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,48 +1,28 @@
-import java.util.List;
-import java.util.Queue;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
-	static int N;
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		N = Integer.parseInt(br.readLine());
-
-		bw.write(func()+"\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	static int func()
-	{
+		
 		Queue<Integer> q=new LinkedList<>();
+		
+		int N=Integer.parseInt(br.readLine());
+		
 		for(int i=1;i<=N;i++)
 		{
 			q.add(i);
 		}
-		boolean swtch=true;
+		
+		
 		while(q.size()>1)
 		{
-			if(swtch)
-			{
-				q.poll();
-				swtch=false;
-			}
-			else
-			{
-				q.add(q.poll());
-				swtch=true;
-			}
+			q.poll();
+			q.add(q.poll());
+			//System.out.println(q.toString());
 		}
-		return q.poll();
+		System.out.println(q.poll());
 	}
-
 }
