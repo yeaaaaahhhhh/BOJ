81ca4ed3dde11419733943d983c539e29336de76
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2023-08-07 17:38:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 728 ms, Memory: 296508 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,52 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
-public class Main
-{
-	static int N;
-	static int M;
-	static Queue<Integer> q=new LinkedList();
-	public static void main(String[] args)
-	{
-		try{
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			String[] tmp=br.readLine().split(" ");
-			N=Integer.parseInt(tmp[0]);
-			M=Integer.parseInt(tmp[1]);
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] tmp = br.readLine().split(" ");
+
+		int N = Integer.parseInt(tmp[0]);
+		int M = Integer.parseInt(tmp[1]);
+
+		Queue<Integer> list = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			list.add(i);
 		}
-		catch(IOException e)
-		{
-			System.out.println(e.getMessage());
-		}
-		init();
-		func();
-	}
-	static void init()
-	{
-		for(int i=0;i<N;i++)
-		{
-			q.add(i+1);
-		}
-	}
-	static void func()
-	{
 		System.out.print("<");
-		while(q.size()!=1)
-		{
-			for(int i=0;i<M;i++)
-			{
-				if(i==M-1)
-				{
-					System.out.print(q.peek()+", ");
-					q.poll();
+		while (!list.isEmpty()) {
+			for (int i = 0; i < M; i++) {
+				if (i == M - 1) {
+					if (list.size() == 1) {
+						System.out.print(list.poll());
+					} else {
+						System.out.print(list.poll() + ", ");
+					}
+					break;
 				}
-				else
-				{
-					q.add(q.peek());
-					q.poll();
-				}
+				list.add(list.poll());
 			}
 		}
-		System.out.print(q.peek()+">");
+		System.out.print(">");
 	}
 }
