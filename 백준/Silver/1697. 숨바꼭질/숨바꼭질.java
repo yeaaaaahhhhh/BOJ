c187de0925e5871fa93a729f64cf98e16dc4307b
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2023-08-18 15:44:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,50 +1,41 @@
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int X;
-	static int Y;
-
+public class Main{
+	static int answer;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] tmp = br.readLine().split(" ");
-		X = Integer.parseInt(tmp[0]);
-		Y = Integer.parseInt(tmp[1]);
-
-		bw.write(func() + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());		//수빈이 위치
+		int k = Integer.parseInt(st.nextToken());		//동생 위치
+		answer = Integer.MAX_VALUE;
+		
+		dfs(n, k, 0);
+		System.out.println(answer);
 	}
-
-	static int func() {
-		if (Y < X) {
-			return X - Y;
-		} else {
-			int[] grd = new int[Y+1];
-			for(int i=0;i<X;i++)
-			{
-				grd[i]=Math.abs(X-i);
+	
+	public static void dfs(int n, int k, int count) {		//수빈이위치, 동생위치, 카운트
+		if( n >= k ) {
+			count += n - k;
+			if( answer > count ) {
+				answer = count;
 			}
-			for (int i = X+1; i <= Y; i++) {
-				if(i%2==0)
-				{
-					grd[i]=Math.min(grd[i/2]+1, grd[i-1]+1);
-				}
-				else
-					grd[i]=Math.min(grd[i-1]+1,grd[i/2+1]+2);
-			}
-			return grd[Y];
+			return;
 		}
-
+		
+		dfs(n, n, count + k - n);
+		if( n == 0 ) {
+			n = 1;
+			count++;
+		}
+		
+		if( k % 2 == 1 ) {
+			dfs(n, k + 1, count + 1);
+			dfs(n, k - 1, count + 1);
+		}		
+		else {
+			dfs(n, k / 2, count + 1);
+		}
 	}
 }
