5fb9d8021db684bb40f2b5c080b633fe060bbaac
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2024-03-10 17:54:24 +0900

    [level 3] Title: 양과 늑대, Time: 1.19 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,80 +1,84 @@
 import java.util.*;
 class Solution {
-    static int n;
-    static int[][] route;
-    static int max;
-    static boolean[][] visited;
-    static int[] pinfo;
-    
     public int solution(int[] info, int[][] edges) {
         int answer = 0;
-        n=info.length;
-        route=new int[n][3];
-        visited=new boolean[1<<n][n];
-        pinfo=info;
-        int len=(1<<n)-1;
-        //System.out.println((1<<n)-1);
         
-        for(int i=0;i<n;i++)
+        ArrayList<Integer>[] map=new ArrayList[info.length];
+        
+        for(int i=0;i<info.length;i++)
         {
-            Arrays.fill(route[i],-1);
+            map[i]=new ArrayList<Integer>();
         }
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<edges.length;i++)
         {
             int par=edges[i][0];
             int chd=edges[i][1];
-            
-            route[chd][0]=par;
-            
-            //왼쪽 자식 없음
-            if(route[par][1]==-1)
-            {
-                route[par][1]=chd;
-            }
-            else
-            {
-                route[par][2]=chd;
-            }
-            
+            map[par].add(chd);
+            map[chd].add(par);
         }
         
-        dfs(0,1,0,1);
-        return max;
+        boolean[][] visited=new boolean[1<<info.length][info.length];
+        
+        Queue<State> q=new LinkedList();
+        q.add(new State(1,0,0,1));
+        visited[0][0]=true;
+        
+        while(!q.isEmpty())
+        {
+            State cur=q.poll();
+            // System.out.println(cur);
+            if(cur.s>answer)
+            {
+                answer=cur.s;
+            }
+            for(int chd:map[cur.loc])
+            {
+
+                State node=new State(cur.s,cur.w,chd,(cur.vst|(1<<chd)));
+                //언제든 방문한적 없음
+                if((cur.vst&(1<<chd))==0){
+                    if(info[chd]==0)
+                    {
+                        node.s++;   
+                    }
+                    else
+                    {
+                        node.w++;
+                    }
+                }
+                
+                if(visited[node.vst][chd]||node.s<=node.w)
+                {
+                    continue;
+                }
+                
+                q.add(node);
+                visited[node.vst][chd]=true;
+            }
+        }
+        return answer;
     }
-    static void dfs(int node,int sheep,int wolf,int status)
+}
+class State{
+    int s=0;
+    int w=0;
+    int loc;
+    int vst;
+    public State(int s,int w,int loc)
+    {   
+        this.s=s;
+        this.w=w;
+        this.loc=loc;
+    }
+    public State(int s,int w,int loc, int vst)
     {
-        if(sheep<=wolf)
-        {
-            return;
-        }
-        //System.out.println(node+" "+sheep+" "+wolf+" "+Integer.toBinaryString(status));
-        int tinfo=pinfo[node];
-        pinfo[node]=-1;
-        if(max<sheep)
-        {
-            max=sheep;
-        }
-        for(int i=0;i<3;i++)
-        {
-            int dest=route[node][i];
-            if(dest==-1||visited[status][dest])
-            {
-                continue;    
-            }
-            visited[status][dest]=true;
-            if(pinfo[dest]==0)//양
-            {
-                dfs(dest,sheep+1,wolf,status+(1<<dest));   
-            }
-            else if(pinfo[dest]==1)//늑대
-            {
-                dfs(dest,sheep,wolf+1,status);
-            }
-            else
-            {
-                dfs(dest,sheep,wolf,status);
-            }
-        }
-        pinfo[node]=tinfo;
+        this.s=s;
+        this.w=w;
+        this.loc=loc;
+        this.vst=vst;
+    }
+    public String toString()
+    {
+        return this.s+" "+this.w+" "+this.loc+" "+this.vst;
     }
 }
