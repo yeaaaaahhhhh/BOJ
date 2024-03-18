103d44496e01352e2e682146be234e104ae08d9f
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2024-03-18 21:39:02 +0900

    [Gold V] Title: 최소비용 구하기, Time: 596 ms, Memory: 70320 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,77 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
+		int N=Integer.parseInt(br.readLine());
+		int M=Integer.parseInt(br.readLine());
+		
 		ArrayList<int[]>[] list=new ArrayList[N];
 		
+		int[][] price=new int[N][N];
 		for(int i=0;i<N;i++)
 		{
+			Arrays.fill(price[i],100000001);
 			list[i]=new ArrayList<int[]>();
+			price[i][i]=0;
 		}
-		for (int i = 0; i < M; i++) {
-			String[] tmp = br.readLine().split(" ");
+		
+		for(int i=0;i<M;i++)
+		{
+			String[] arr=br.readLine().split(" ");
 			
-			int A=Integer.parseInt(tmp[0])-1;
-			int B=Integer.parseInt(tmp[1])-1;
-			int V=Integer.parseInt(tmp[2]);
+			int A=Integer.parseInt(arr[0])-1;
+			int B=Integer.parseInt(arr[1])-1;
+			int V=Integer.parseInt(arr[2]);
 			
 			list[A].add(new int[] {B,V});
 		}
 		
-		String[] tmp=br.readLine().split(" ");
-		
-		int S=Integer.parseInt(tmp[0])-1;
-		int E=Integer.parseInt(tmp[1])-1;
-		
-		int[] res=new int[N];
-		Arrays.fill(res, 100000000);
-		res[S]=0;
+		String[] arr=br.readLine().split(" ");
+		int S=Integer.parseInt(arr[0])-1;
+		int D=Integer.parseInt(arr[1])-1;
 		
 		boolean[] visited=new boolean[N];
-		
-		PriorityQueue<int[]> q=new PriorityQueue<int[]>((o1,o2)->{
-			return o1[1]-o2[1];
-		});
-		q.add(new int[] {S,0});
-		visited[S]=true;
-		
-		while(!q.isEmpty())
-		{
-			int[] cur=q.poll();
-			//여기 방문체크가 들어가야 모든 노드 기준으로 맵의 최소값 계산을 한번씩 
-			visited[cur[0]]=true;
-			
-			if(cur[0]==E)
+		PriorityQueue<Integer> pq=new PriorityQueue<Integer>(new Comparator<Integer>() {
+			public int compare(Integer a,Integer b)
 			{
-				break;
+				return price[S][a]-price[S][b];
 			}
+		});
+		pq.add(S);
+		
+		while(!pq.isEmpty())
+		{
+			int cur=pq.poll();
+//			System.out.println(cur);
 			
-			for(int[] info:list[cur[0]])
+			if(visited[cur])
 			{
-				if(!visited[info[0]]&&res[info[0]]>res[cur[0]]+info[1])
+				continue;
+			}
+			visited[cur]=true;
+			
+			for(int[] route:list[cur])
+			{
+				int ds=route[0];
+				int v=route[1];
+				if(price[S][cur]+v<price[S][ds])
 				{
-					//여기에 방문체크가 들어가면 시작점 기준으로만 최소값 계산
-					res[info[0]]=res[cur[0]]+info[1];
-					q.add(new int[] {info[0],res[info[0]]});
+					price[S][ds]=price[S][cur]+v;
+					pq.add(ds);
 				}
 			}
 		}
 		
-//		System.out.println(Arrays.toString(res));
-		System.out.println(res[E]);
+		System.out.println(price[S][D]);
 		
 	}
-
 }
