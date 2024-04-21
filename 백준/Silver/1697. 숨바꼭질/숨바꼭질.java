bac572b11a5fc8b43c401e17c9ee067fb4109ade
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2024-04-21 23:23:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 16452 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,41 +1,47 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main{
-	static int answer;
-	
+public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());		//수빈이 위치
-		int k = Integer.parseInt(st.nextToken());		//동생 위치
-		answer = Integer.MAX_VALUE;
 		
-		dfs(n, k, 0);
-		System.out.println(answer);
-	}
-	
-	public static void dfs(int n, int k, int count) {		//수빈이위치, 동생위치, 카운트
-		if( n >= k ) {
-			count += n - k;
-			if( answer > count ) {
-				answer = count;
+		String[] tmp=br.readLine().split(" ");
+		
+		int N=Integer.parseInt(tmp[0]);
+		int K=Integer.parseInt(tmp[1]);
+		
+		int[] arr=new int[100001];
+		Arrays.fill(arr, 100001);
+		
+		Queue<Integer> q=new LinkedList<Integer>();
+		q.add(N);
+		arr[N]=0;
+		while(!q.isEmpty())
+		{
+			int cur=q.poll();
+			if(cur==K)
+			{
+				break;
 			}
-			return;
+			
+			if(cur+1<100001&&arr[cur+1]==100001)
+			{
+				arr[cur+1]=arr[cur]+1;
+				q.add(cur+1);
+			}
+			if(cur-1>=0&&arr[cur-1]==100001)
+			{
+				arr[cur-1]=arr[cur]+1;
+				q.add(cur-1);
+			}
+			if(cur*2<100001&&arr[cur*2]==100001)
+			{
+				arr[cur*2]=arr[cur]+1;
+				q.add(cur*2);
+			}
 		}
-		
-		dfs(n, n, count + k - n);
-		if( n == 0 ) {
-			n = 1;
-			count++;
-		}
-		
-		if( k % 2 == 1 ) {
-			dfs(n, k + 1, count + 1);
-			dfs(n, k - 1, count + 1);
-		}		
-		else {
-			dfs(n, k / 2, count + 1);
-		}
+		System.out.println(arr[K]);
 	}
+
 }
