3add4de7da6c769a0ddde78cde30f38770eaa62c
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2024-05-05 00:12:32 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 20704 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,27 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N=Integer.parseInt(br.readLine());
-		int M=Integer.parseInt(br.readLine());
-		String s=br.readLine();
-		String find="IO".repeat(N)+"I";
-//		System.out.println(find);
-		int idx=0;
-		int res=0;
-		
-		while((idx=s.indexOf(find,idx))!=-1)
-		{
-//			System.out.println(idx);
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+
+		StringBuilder sb = new StringBuilder();
+
+		int len = N * 2 + 1;
+
+		int mat = 0;
+		int idx = 0;
+		int res = 0;
+		while (idx < S.length()) {
+			if ((mat % 2 == 0 && S.charAt(idx) == 'I') || (mat % 2 == 1 && S.charAt(idx) == 'O')) {
+				mat++;
+			} else {
+				if (mat != 0) {
+					if (mat == 1) {
+						mat--;
+					} else {
+						mat -= 2;
+					}
+					continue;
+				}
+			}
+//			System.out.println(S.charAt(idx) + " " + idx + " " + mat);
+			if (mat == len) {
+				mat -= 2;
+				res++;
+//				System.out.println("found");
+			}
 			idx++;
-			res++;
 		}
-		
 		System.out.println(res);
 	}
-
 }
