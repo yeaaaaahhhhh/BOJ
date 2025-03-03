3b3585090b1384e8160f7c362b891d38f47ed3fe
김예현 Yehyeon Kim <72369871+yeaaaaahhhhh@users.noreply.github.com>
2025-03-03 20:54:15 +0900

    Time: 0 ms (100%), Space: 19.7 MB (43.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,17 +1,16 @@
 void moveZeroes(int* nums, int numsSize) {
-	
-	int zeroCnt = 0;
 	int idx = 0;
 	for (int i = 0;i < numsSize;i++)
 	{
 		int cur = nums[i];
 		if(cur!=0)
 		{
-			nums[idx++] = cur;
+			if(idx!=i)
+			{
+				nums[i] = nums[idx];
+				nums[idx] = cur;
+			}
+			idx++;
 		}
-	}
-	while (idx < numsSize)
-	{
-		nums[idx++] = 0;
 	}
 }
